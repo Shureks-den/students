57f01c0370be1bf83bfded0724d79e9feb4e938e
alexa <alexander.klonov@mail.ru>
2020-09-16 21:56:31 +0300

    finish project

--- main.cpp
+++ main.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 
-using std::string;
 
 class Student {
 public:
     Student() {
-        name = new char[20];
-        surname = new char[20];
-        group = new char[10];
-        marks = new int [5];
+        name = new char[20]{'\0'};
+        surname = new char[20]{'\0'};
+        group = new char[10]{'\0'};
+        marks = new int[5];
         average = 0;
     };  // конструктор по умолчанию
-    void set_info(string info) {
+    Student(const Student & copy){
+        this->average=copy.average;
+        strcpy(this->surname,copy.surname);
+        strcpy(this->name,copy.name);
+        strcpy(this->group,copy.group);
+        for (int i = 0; i <5; ++i) {
+            this->marks[i] = copy.marks[i];
+        }
+    }
+    void set_info(std::string info) {
         int i = 0;
         int k = 0;
         while (info[i] != ' ') {
             surname[k++] = info[i++];
         }
-        surname[k]='\0';
+        surname[k] = '\0';
         i++;
         k = 0;
         while (info[i] != ' ') {
             name[k++] = info[i++];
         }
-        name[k]='\0';
+        name[k] = '\0';
         i++;
         k = 0;
         while (info[i] != ' ') {
             group[k++] = info[i++];
         }
-        group[k]='\0';
+        group[k] = '\0';
         i++;
         for (k = 0; k < 5; k++) {
             marks[k] = info[i] - '0';
@@ -42,7 +51,7 @@ public:
     void get_info() {
         std::cout << surname << " " << name << " " << group;
         for (int i = 0; i < 5; i++)
-            std::cout << " "<< marks[i] << " ";
+            std::cout << " " << marks[i] << " ";
         std::cout << std::endl;
     }
 
@@ -55,12 +64,15 @@ public:
 
     }
 
+
     ~Student() {
         delete[] name;
         delete[] surname;
         delete[] group;
         delete[] marks;
     };
+
+    friend void sort(Student *lst, int count);
 
 private:
     char *name;
@@ -70,10 +82,23 @@ private:
     float average;
 };
 
+void sort(Student *lst, int count) {
+    Student b;
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < count -1; j++) {
+            if (lst[j].average > lst[j + 1].average) {
+                b = lst[j]; // создали дополнительную переменную
+                lst[j] = lst[j + 1]; // меняем местами
+                lst[j + 1] = b; // значения элементов
+            }
+        }
+    }
+}
+
 int main() {
     int sCount = 0;
     Student *bomonka = new Student[20];
-    string info;
+    std::string info;
     std::ifstream F("studentsList.txt");
     if (F) {
         while (std::getline(F, info)) {
@@ -81,9 +106,13 @@ int main() {
             sCount++;
         }
         F.close();
-
         std::cout << "Number of students is " << sCount << std::endl;
-
+        for (int i = 0; i < sCount; i++) {
+            bomonka[i].get_info();
+            bomonka[i].get_avrg();
+        }
+        std::cout << "sorted list" << std::endl;
+        sort(bomonka, sCount);
         for (int i = 0; i < sCount; i++) {
             bomonka[i].get_info();
             bomonka[i].get_avrg();
